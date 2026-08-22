6ee9e50dace8f473f669aa8b2f381284062dfabf
高艳兵 <yanbing.gao@aliyun.com>
2026-08-22 15:43:51 +0800

    feat: register default link commands

--- packages/core/src/command/defaults.ts
+++ packages/core/src/command/defaults.ts
@@ -1,6 +1,7 @@
 import { mergeBlockCommand, splitBlockCommand } from "./block";
 import { BOOLEAN_MARK_COMMANDS } from "./mark";
 import { createCommandRegistry, type CommandRegistry } from "./registry";
+import { LINK_COMMANDS } from "./link";
 import { deleteSelectionCommand, insertTextCommand } from "./text";
 import { TEXT_STYLE_COMMANDS } from "./text-style";
 import type { Command } from "./types";
@@ -8,6 +9,7 @@ import type { Command } from "./types";
 export const DEFAULT_COMMANDS: readonly Command[] = [
   ...BOOLEAN_MARK_COMMANDS,
   ...TEXT_STYLE_COMMANDS,
+  ...LINK_COMMANDS,
   deleteSelectionCommand,
   insertTextCommand,
   mergeBlockCommand,
